3a5720931785989dfa442c2e7d7ab6d75e4c8b3f
marsvo1ta <euromobilco@gmail.com>
2025-05-17 01:04:05 +0300

    svg reload

--- src/components/CryptoChart/CryptoChart.jsx
+++ src/components/CryptoChart/CryptoChart.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { Line } from 'react-chartjs-2';
+import reloadIcon from '../../assets/reload.svg';
+import './CryptoChart.css'
 import {
   Chart as ChartJS,
   CategoryScale,
@@ -17,12 +19,13 @@ ChartJS.register(CategoryScale, LinearScale, LineElement, PointElement, Tooltip,
 
 const CryptoChart = () => {
   const [chartData, setChartData] = useState(null);
+  const [loading, setLoading] = useState(false);
 
-  useEffect(() => {
+  const fetchChartData = () => {
+    setLoading(true);
     axios.get('https://api-aio.alwaysdata.net/crypto/prices?symbol=BTCUSDT&limit=50')
       .then(response => {
-        const data = response.data.reverse(); // Реверс, щоб час ішов зліва направо
-
+        const data = response.data.reverse(); // Час зліва направо
         const labels = data.map(item => item.timestamp);
         const prices = data.map(item => item.close);
 
@@ -42,8 +45,25 @@ const CryptoChart = () => {
       })
       .catch(error => {
         console.error('Помилка при завантаженні даних:', error);
-      });
+      })
+      .finally(() => setLoading(false));
+  };
+
+  useEffect(() => {
+    fetchChartData();
   }, []);
+
+  const handleReload = () => {
+    setLoading(true);
+    axios.post('https://api-aio.alwaysdata.net/crypto/binance/import?symbol=BTCUSDT&interval=5m&limit=50')
+      .then(() => {
+        fetchChartData();
+      })
+      .catch(error => {
+        console.error('Помилка при оновленні даних:', error);
+        setLoading(false);
+      });
+  };
 
   const options = {
     responsive: true,
@@ -57,6 +77,7 @@ const CryptoChart = () => {
         type: 'time',
         time: {
           unit: 'minute',
+          stepSize: 5, 
           tooltipFormat: 'yyyy-MM-dd HH:mm'
         },
         title: {
@@ -65,17 +86,21 @@ const CryptoChart = () => {
         }
       },
       y: {
+        position: 'right',
         title: {
           display: true,
           text: 'Ціна (USDT)'
         }
       }
     }
-  };
+  };  
 
   return (
     <div>
       <h2>Графік ціни BTC/USDT</h2>
+      <button className='reload-button button' onClick={handleReload} disabled={loading}>
+      {loading ? 'Оновлення...' : <img src={reloadIcon} alt="Reload" width={24} height={24} />}
+      </button>
       {chartData ? <Line data={chartData} options={options} /> : <p>Завантаження даних...</p>}
     </div>
   );
